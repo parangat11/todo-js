b391dcc5ad161db488d87a0016fff09a7e6f95b9
parangat11 <parangataditya001@gmail.com>
2024-12-05 11:02:35 +0530

    Add a few useful scripts

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  "scripts": {
+    "build": "webpack",
+    "dev": "webpack serve",
+    "deploy": "git subtree push --prefix dist origin gh-pages"
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
